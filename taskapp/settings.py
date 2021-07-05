c0d333ba48634be8f723c79c649b0f8de592db60
Nikita Khmel <andersonfamily23@gmail.com>
2021-07-05 18:45:28 +0300

    Update requirements.txt.

--- taskapp/settings.py
+++ taskapp/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'shop',
-    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -128,7 +127,5 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = '/'
