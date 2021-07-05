50c44fc12c3ffea9eec747b72f70c3ae98dc3b36
Nikita Khmel <andersonfamily23@gmail.com>
2021-07-05 15:39:31 +0300

    Add migration (ProductPriceChange), Homepage at '/' url.

--- taskapp/urls.py
+++ taskapp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('shop/', include('shop.urls')),
+    path('', include('shop.urls')),
     path('accounts/', include('django.contrib.auth.urls'))
 ]
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
